9d6ded32c3b72cbdf3edd6ec33425093ae016666
Pigmassacre <gulligaolle@gmail.com>
2013-04-12 18:32:21 +0200

    Added the first camera draft, untested so may not work at all (the Capture button will not work as they have no listeners)

--- BarcodeScanner/src/com/github/barcodescanner/MainActivity.java
+++ BarcodeScanner/src/com/github/barcodescanner/MainActivity.java
@@ -8,24 +8,23 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    public void enterCamera(View view) {
-    	// TODO Start the camera activity when the button is pressed.
-    	Intent intent = new Intent(this, CameraActivity.class);
+	public void enterCamera(View view) {
+		// TODO Start the camera activity when the button is pressed.
+		Intent intent = new Intent(this, CameraActivity.class);
 		startActivity(intent);
-    }
-    
+	}
+
 }
