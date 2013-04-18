4e6bfc60806d05630c7aaf34af99ad6030160a18
crillesvensson <crillesvensson22@gmail.com>
2013-04-18 13:40:54 +0200

    implemented saving Image file

--- BarcodeScanner/src/com/github/barcodescanner/CameraActivity.java
+++ BarcodeScanner/src/com/github/barcodescanner/CameraActivity.java
@@ -1,8 +1,17 @@
 package com.github.barcodescanner;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -52,12 +61,50 @@ public class CameraActivity extends Activity {
 
 		@Override
 		public void onPictureTaken(byte[] data, Camera camera) {
-			bcAnalyzer = new BCanalyzer(data);
-			System.out.println(bcAnalyzer.getWidth());
+			//bcAnalyzer = new BCanalyzer(data);
+			//System.out.println(bcAnalyzer.getWidth());
+			
 			System.out.println("BILD SPARAD!!!");
-		}
+		       File pictureFile = getOutputMediaFile();
+		        if (pictureFile == null){
+		            Log.d("TEST", "Error creating media file, check storage permissions: ");
+		            return;
+		        }
+
+		        try {
+		            FileOutputStream fos = new FileOutputStream(pictureFile);
+		            fos.write(data);
+		            fos.close();
+		        } catch (FileNotFoundException e) {
+		            Log.d("TEST", "File not found: " + e.getMessage());
+		        } catch (IOException e) {
+		            Log.d("TEST", "Error accessing file: " + e.getMessage());
+		        }
+		    }
 
 	};
+	
+	/** Create a File for saving the image */
+    private static File getOutputMediaFile(){
+ 
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                  Environment.DIRECTORY_PICTURES), "MyCameraApp");
+ 
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+ 
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+            "IMG_"+ timeStamp + ".jpg");
+ 
+        return mediaFile;
+    }
 
 	// instead of adding a listener we can add "android:onClick="methodName" to the activity_camera.xml file
 	// then we can just add the mCamera.takePicture(bla..) to that method. Saves us some code and I think that is
@@ -66,6 +113,6 @@ public class CameraActivity extends Activity {
 		System.out.println("Bild Tagen");
 		Intent intent = new Intent(this, ProductActivity.class);
 		startActivity(intent);
-		//mCamera.takePicture(null, null, picture);		
+		mCamera.takePicture(null, null, picture);		
 	}
 }
