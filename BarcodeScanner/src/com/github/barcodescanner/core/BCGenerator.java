cfa7e7665019bb382d4621f9816195a8a28f7779
Oted <herr.olle@gmail.com>
2013-05-21 11:19:43 +0300

    added code to BCGenerator

--- BarcodeScanner/src/com/github/barcodescanner/core/BCGenerator.java
+++ BarcodeScanner/src/com/github/barcodescanner/core/BCGenerator.java
@@ -1,5 +1,34 @@
 package com.github.barcodescanner.core;
 
-public class BCGenerator {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BCGenerator{
+	
+	private List<Integer> unNormalized = new ArrayList<Integer>();
+	private List<Integer> normalized = new ArrayList<Integer>();
+	
+	public BCGenerator(){}
+	//11000
+	//23
+	public void generate(List<Integer> line){
+		int count = 1;
+		
+		for (int i = 0; i < line.size() - 1 ; i++){ 
+			if(line.get(i)==line.get(i+1)){
+				count++;
+			}
+			else{
+				unNormalized.add(count);
+				count = 1;
+			}
+		}
+		unNormalized.add(count);
+	}
+	
+	private void normalize(){
+		
+		
+	}
 
 }
