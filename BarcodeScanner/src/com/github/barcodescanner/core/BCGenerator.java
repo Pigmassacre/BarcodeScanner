8fbcb9ce357e2afc5c55eb2fa7a13409eea1be8f
Pigmassacre <gulligaolle@gmail.com>
2013-05-22 10:53:38 +0200

    stuff.

--- BarcodeScanner/src/com/github/barcodescanner/core/BCGenerator.java
+++ BarcodeScanner/src/com/github/barcodescanner/core/BCGenerator.java
@@ -25,15 +25,14 @@ public class BCGenerator{
 		return unNormalized;
 	}
 	
-	private String normalize(){
-		StringBuffer s = null;
+	public String normalize(){
+		StringBuffer s = new StringBuffer();
 		if (unNormalized==null){
 			return "No unnormalized data is set";
 		}
-		else {
-			List<Integer> normalized;
-			int lengths = unNormalized.get(0) + unNormalized.get(1) + unNormalized.get(unNormalized.size()-1) + unNormalized.get(unNormalized.size()-2);
-			float division = lengths / 4;
+		else {			
+			int lengths = unNormalized.get(0) + unNormalized.get(1) + unNormalized.get(unNormalized.size()-1) + unNormalized.get(unNormalized.size()-2) + unNormalized.get(unNormalized.size()/2);
+			float division = lengths / 5;
 			
 			for (int i = 0; i < unNormalized.size(); i++){
 				int value = Math.round(unNormalized.get(i)/division);
