a25b20218458aa40286b6bafaa36f9c51810d43d
Rasmus Letterkrantz <Rasmus.Letterkrantz@gmail.com>
2013-05-24 15:50:17 +0200

    final version of Database view

--- BarcodeScanner/src/com/github/barcodescanner/activities/DatabaseActivity.java
+++ BarcodeScanner/src/com/github/barcodescanner/activities/DatabaseActivity.java
@@ -10,20 +10,14 @@ import com.github.barcodescanner.core.Product;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
@@ -32,145 +26,103 @@ public class DatabaseActivity extends Activity {
 	@SuppressWarnings("unused")
 	private static final String TAG = "DatabaseActivity";
 	DatabaseHelper db;
-	List<String[]> items = new ArrayList();
+	private List<Product> items = new ArrayList<Product>();
+	private ListView list;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_database);
-		ListView list = (ListView) findViewById(R.id.list);
-		 
-		String[] item = {"item1Name", "item1Price","item1Id"};
-		String[] item2 = {"item2Name", "item2Price","item2Id"};
-		items.add(item);
-		items.add(item2);
+
+		// The database
+		DatabaseHelperFactory.init(this);
+		db = DatabaseHelperFactory.getInstance();
+
+		// set the ListView of items
+		list = (ListView) findViewById(R.id.list);
+		items = db.getProducts();
 		SpecialAdapter adapter = new SpecialAdapter(this, items);
 		list.setAdapter(adapter);
 
-		DatabaseHelperFactory.init(this);
-		db = DatabaseHelperFactory.getInstance();
-		
-		List<Product> products = db.getProducts();
-		List<String> tempList = new ArrayList<String>();
-		if (products.size() > 0) {
-			for (int i = 0; i < products.size(); i++) {
-				tempList.clear();
-
-				tempList.add(products.get(i).getBarcode());
-				tempList.add(products.get(i).getName());
-				tempList.add(Integer.toString(products.get(i).getPrice()));
-
-				addRow(tempList);
-			}
-		}
-
 	}
-	
+
 	static class ViewHolder {
-	    TextView name, id, price;
-	}
-	
-	public void deleteClick(View v) {
-	    ImageButton button = (ImageButton) v;
-	    TableRow row = (TableRow)button.getParent();
-	    TextView textChild = (TextView) row.getChildAt(0);
-	    String text = textChild.getText().toString();
-	    text.split("id");
+		TextView name, id, price;
 	}
 
-	private void addRow(List<String> values) {
-		TableLayout table = (TableLayout) findViewById(R.id.list);
-		TableRow row = new TableRow(this);
-		row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
-				LayoutParams.WRAP_CONTENT));
-		TextView textView;
-		for (int i = 0; i < values.size(); i++) {
-			textView = generateCell(values.get(i).toString());
-			row.addView(textView);
-			row.setGravity(Gravity.CENTER);
-		}
-		table.addView(row, new TableLayout.LayoutParams(
-				LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+	public void clickDelete(View v) {
+		ImageButton button = (ImageButton) v;
+		TableRow row = (TableRow) button.getParent();
+		TextView idView = (TextView) row.getChildAt(2);
+		String id = idView.getText().toString();
+		db.removeProduct(id);
+
+		items = db.getProducts();
+		SpecialAdapter adapter = new SpecialAdapter(this, items);
+		list.setAdapter(adapter);
 	}
-	
-	/*
-	 * 
-	 * 
-	private void removeRow() {
-		// TODO
-	}*/
-	
-	
-	
-	private TextView generateCell(String text) {
-		// TODO Fix this, it looks ugly on the device
-		TextView nameView = new TextView(this);
-		nameView.setText(text);
-		nameView.setGravity(Gravity.CENTER_HORIZONTAL);
-		nameView.setTypeface(null, Typeface.BOLD);
-		//nameView.setLayoutParams(new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT, 1f));
-		return nameView;
-	}
-	
+
+
 	private class SpecialAdapter extends BaseAdapter {
-		//Defining the background color of rows. The row will alternate between green light and green dark.
+		// Defining the background color of rows. The row will alternate between
+		// grey light and grey dark.
 		private int[] colors = new int[] { 0xAA999999, 0xAA7d7d7d };
 		private LayoutInflater mInflater;
-		 
-		//The variable that will hold our text data to be tied to list.
-		private List<String[]> data;
-		 
-		public SpecialAdapter(Context context, List<String[]> items) {
-		    mInflater = LayoutInflater.from(context);
-		    this.data = items;
+
+		// The variable that will hold our text data to be tied to list.
+		private List<Product> data;
+
+		public SpecialAdapter(Context context, List<Product> items) {
+			mInflater = LayoutInflater.from(context);
+			this.data = items;
 		}
-		 
+
 		@Override
 		public int getCount() {
-		    return data.size();
+			return data.size();
 		}
-		 
+
 		@Override
 		public Object getItem(int position) {
-		    return position;
+			return position;
 		}
-		 
+
 		@Override
 		public long getItemId(int position) {
-		    return position;
+			return position;
 		}
-		 
-		//A view to hold each row in the list
+
+		// A view to hold each row in the list
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-		 
-		// A ViewHolder keeps references to children views to avoid unneccessary calls
-		// to findViewById() on each row.
-		ViewHolder holder;
-		 
-		if (convertView == null) {
-		    convertView = mInflater.inflate(R.layout.row, null);
-		 
-		    holder = new ViewHolder();
-		    holder.name = (TextView) convertView.findViewById(R.id.name);
-		    holder.price = (TextView) convertView.findViewById(R.id.price);
-		    holder.id = (TextView) convertView.findViewById(R.id.id);
-		    convertView.setTag(holder);
-		} else {
-		    holder = (ViewHolder) convertView.getTag();
+
+			// A ViewHolder keeps references to children views to avoid
+			// unneccessary calls
+			// to findViewById() on each row.
+			ViewHolder holder;
+
+			if (convertView == null) {
+				convertView = mInflater.inflate(R.layout.row, null);
+
+				holder = new ViewHolder();
+				holder.name = (TextView) convertView.findViewById(R.id.name);
+				holder.price = (TextView) convertView.findViewById(R.id.price);
+				holder.id = (TextView) convertView.findViewById(R.id.id);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			// Bind the data efficiently with the holder.
+			holder.name.setText(data.get(position).getName());
+			holder.price.setText("" + data.get(position).getPrice());
+			holder.id.setText(data.get(position).getBarcode());
+
+			// Set the background color depending of odd/even colorPos result
+			int colorPos = position % colors.length;
+			convertView.setBackgroundColor(colors[colorPos]);
+
+			return convertView;
 		}
-		    // Bind the data efficiently with the holder.
-		    holder.name.setText(data.get(position)[0]);
-		    holder.price.setText(data.get(position)[1]);
-		    holder.id.setText(data.get(position)[2]);
-		 
-		    //Set the background color depending of  odd/even colorPos result
-		    int colorPos = position % colors.length;
-		    convertView.setBackgroundColor(colors[colorPos]);
-		 
-		   return convertView;
-		}
-		}
+	}
 }
-
-
