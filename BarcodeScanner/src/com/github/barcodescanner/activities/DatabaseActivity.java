6498f255904982289aa435d5d03d0271fab7cb52
Rasmus Letterkrantz <Rasmus.Letterkrantz@gmail.com>
2013-05-24 12:15:01 +0200

    modified teh database view

--- BarcodeScanner/src/com/github/barcodescanner/activities/DatabaseActivity.java
+++ BarcodeScanner/src/com/github/barcodescanner/activities/DatabaseActivity.java
@@ -9,11 +9,17 @@ import com.github.barcodescanner.core.DatabaseHelperFactory;
 import com.github.barcodescanner.core.Product;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.Window;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -23,12 +29,16 @@ public class DatabaseActivity extends Activity {
 	@SuppressWarnings("unused")
 	private static final String TAG = "DatabaseActivity";
 	DatabaseHelper db;
-
+	String[] items = { "ExampleName    ExamplePrice   ExampleId", "", "ExampleName    ExamplePrice   ExampleId", "ExampleName    ExamplePrice   ExampleId"};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_database);
+		ListView list = (ListView) findViewById(R.id.list);
+		 
+		SpecialAdapter adapter = new SpecialAdapter(this, items);
+		list.setAdapter(adapter);
 
 		DatabaseHelperFactory.init(this);
 		db = DatabaseHelperFactory.getInstance();
@@ -48,9 +58,13 @@ public class DatabaseActivity extends Activity {
 		}
 
 	}
+	
+	static class ViewHolder {
+	    TextView text;
+	}
 
 	private void addRow(List<String> values) {
-		TableLayout table = (TableLayout) findViewById(R.id.products_table);
+		TableLayout table = (TableLayout) findViewById(R.id.list);
 		TableRow row = new TableRow(this);
 		row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
 				LayoutParams.WRAP_CONTENT));
@@ -58,6 +72,7 @@ public class DatabaseActivity extends Activity {
 		for (int i = 0; i < values.size(); i++) {
 			textView = generateCell(values.get(i).toString());
 			row.addView(textView);
+			row.setGravity(Gravity.CENTER);
 		}
 		table.addView(row, new TableLayout.LayoutParams(
 				LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
@@ -77,4 +92,62 @@ public class DatabaseActivity extends Activity {
 		//nameView.setLayoutParams(new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT, 1f));
 		return nameView;
 	}
+	
+	private class SpecialAdapter extends BaseAdapter {
+		//Defining the background color of rows. The row will alternate between green light and green dark.
+		private int[] colors = new int[] { 0xAA999999, 0xAA7d7d7d };
+		private LayoutInflater mInflater;
+		 
+		//The variable that will hold our text data to be tied to list.
+		private String[] data;
+		 
+		public SpecialAdapter(Context context, String[] items) {
+		    mInflater = LayoutInflater.from(context);
+		    this.data = items;
+		}
+		 
+		@Override
+		public int getCount() {
+		    return data.length;
+		}
+		 
+		@Override
+		public Object getItem(int position) {
+		    return position;
+		}
+		 
+		@Override
+		public long getItemId(int position) {
+		    return position;
+		}
+		 
+		//A view to hold each row in the list
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+		 
+		// A ViewHolder keeps references to children views to avoid unneccessary calls
+		// to findViewById() on each row.
+		ViewHolder holder;
+		 
+		if (convertView == null) {
+		    convertView = mInflater.inflate(R.layout.row, null);
+		 
+		    holder = new ViewHolder();
+		    holder.text = (TextView) convertView.findViewById(R.id.headline);
+		    convertView.setTag(holder);
+		} else {
+		    holder = (ViewHolder) convertView.getTag();
+		}
+		    // Bind the data efficiently with the holder.
+		    holder.text.setText(data[position]);
+		 
+		    //Set the background color depending of  odd/even colorPos result
+		    int colorPos = position % colors.length;
+		    convertView.setBackgroundColor(colors[colorPos]);
+		 
+		   return convertView;
+		}
+		}
 }
+
+
