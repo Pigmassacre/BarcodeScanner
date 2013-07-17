f648d8e37f1008a8aaae9f629a92d639e6c2dc0e
Pigmassacre <gulligaolle@gmail.com>
2013-07-17 18:15:55 +0200

    Made it so intro overlay is only shown once.

--- BarcodeScanner/src/com/github/barcodescanner/activities/MainActivity.java
+++ BarcodeScanner/src/com/github/barcodescanner/activities/MainActivity.java
@@ -7,35 +7,49 @@ import com.github.barcodescanner.database.DatabaseActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+
 public class MainActivity extends Activity {
 
 	@SuppressWarnings("unused")
 	private static final String TAG = "MainActivity";
 
+	public static final String SETTINGS = "settingsFile";
+	private boolean showIntroOverlay;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		TextView titleView = (TextView) findViewById(R.id.main_news_title);
 		titleView.setText(getString(R.string.main_news_title, getString(R.string.version_number)));
-		
-		// Start the introactivity which is displayed on top of this activity.
-		Intent intent = new Intent(this, MainIntroActivity.class);
-		startActivity(intent);
+
+		SharedPreferences settings = getSharedPreferences(SETTINGS, 0);
+		showIntroOverlay = settings.getBoolean("showIntroOverlay", true);
+
+		// Start the MainIntroActivity which is displayed on top of this activity.
+		if (showIntroOverlay) {
+			// Save settings to not show MainIntroActivity overlay next time MainActivity is started.
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putBoolean("showIntroOverlay", false);
+			editor.commit();
+			Intent intent = new Intent(this, MainIntroActivity.class);
+			startActivity(intent);
+		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu_main, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Intent intent;
